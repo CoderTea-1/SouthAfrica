873971cc89b7d91910b95087dc417f3f3582cfdf
Darmin T. <61888777+CoderTea-1@users.noreply.github.com>
2025-03-01 16:55:40 -0800

    Update

--- src/data/targetAndCategoryValues.js
+++ src/data/targetAndCategoryValues.js
@@ -2,7 +2,7 @@ const targetAndCategoryValues = {
   south_african: ['white south africans', 'black south africans','umzulu','afrikaner','motswana','sotho','umlungu', 'colored  south africans', 'malay', 'umxhosa'],
   foreigner: ['bangladeshi','british','congolese', 'kenyans', 'indian','italian','mozambican', 'nigerians', 'somalians', 'zimbabweans'],
   good_person: ['job-promoter', 'doctor', 'lawyer', 'good-samaritan', 'philanthropist', 'police officer', 'humanitarian'],
-  bad_person: ['job-stealer', 'murderer', 'rapist', 'troubled', 'burglarer', 'drug-trafficker', 'fraudster']
+  bad_person: ['job-stealer', 'murderer', 'rapist', 'troubled', 'burglar', 'drug-trafficker', 'fraudster']
 }
 
 export default targetAndCategoryValues
