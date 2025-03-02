2cfe0069ede51ae292c86fde6d880028a0b6d456
Darmin T. <61888777+CoderTea-1@users.noreply.github.com>
2025-03-02 14:10:25 -0800

    Revert "Tracking"
    This reverts commit 9e4d0e02dfe172f03bf95cfc28e99fe0eae9b5b2.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ import ImplicitBiasTest from "./containers/ImplicitBiasTest";
 import DemographicInfo from "./containers/DemographicInfo";
 import "./scss/global.scss";
 import DemographicForm from "./containers/DemographicForm";
-import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
     <Router>
       <div className="nav-and-content">
-        <Analytics />
         <Switch>
           <Route path={`/test/:testId`}>
             <ImplicitBiasTest />
